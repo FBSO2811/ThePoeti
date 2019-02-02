f9a37dfdd4e0e45dcdc9cc555c52980112d3073b
Francesco Stefani <francesco.stefani@outlook.com>
2019-02-02 15:58:10 +0000

    search for famous and amateur

--- app/models/poetry.rb
+++ app/models/poetry.rb
@@ -2,4 +2,13 @@ class Poetry < ApplicationRecord
   def self.search(search)
     where("lower(title) LIKE ? OR lower(author) LIKE ? OR lower(country) LIKE ? OR lower(born) LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
   end
+
+  # def self.amateur(search)
+  #   where("poster != ?","Admin")
+  # end
+
+  # def self.famous(search)
+  #   where("poster = ?","Admin")
+  # end
+
 end
